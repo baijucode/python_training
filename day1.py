b2e3a657edcfb74902fdcff2e08c263f425a5995
Ashmita Baiju <baijuashmita@gmail.com>
2026-03-13 06:26:38 +0545

    done

---
+++ day1.py
@@ -0,0 +1,16 @@
+# print("Hello world")
+# s= "hi"
+# b= 2
+# print ("s")
+# a= 1
+# c= a+b
+# print (s.upper())
+# print(s.lower())
+#ctrl+a ctrl+/ for comment
+# if True:
+#     print("hello")
+# a = 9
+# if (a<5):
+#  print("Hello")
+#  print (a)
+            
